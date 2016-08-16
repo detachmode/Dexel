1d40595a2c77b8e40b33f0eb903d79dfcbb9f7ad
Dennis <dennis.briefkasten@gmail.com>
2016-08-16 12:03:35 +0200

    MyThumb Tutorial Copy pasted

---
+++ SharpFlowDesign/SharpFlowDesign/UserControls/MyThumb.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls.Primitives;
+using System.Windows.Media;
+
+namespace SharpFlowDesign.UserControls
+{
+    public class MyThumb : Thumb
+    {
+        public List<LineGeometry> EndLines { get; private set; }
+        public List<LineGeometry> StartLines { get; private set; }
+
+        public MyThumb() : base()
+        {
+            StartLines = new List<LineGeometry>();
+            EndLines = new List<LineGeometry>();
+        }
+    }
+}
