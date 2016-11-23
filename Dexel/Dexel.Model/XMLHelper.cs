a889278dee75b53eda05a5565927b5a9ae937af9
Dennis Müller <dennis.briefkasten@gmail.com>
2016-11-23 10:34:15 +0100

    commit

---
+++ Dexel/Dexel.Model/XMLHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Dexel.Model
+{
+    public static class XMLHelper
+    {
+
+        public static void SaveToXML<T>(this T obj, string path)
+        {
+
+
+            var xmlSerializer = new XmlSerializer(typeof(T));
+            var stringWriter = new StringWriter();
+            using (var writer = new XmlTextWriter(stringWriter) { Formatting = Formatting.Indented })
+            {
+                xmlSerializer.Serialize(writer, obj);
+                File.WriteAllText(path, stringWriter.ToString());
+            }
+        }
+
+        public static T FromXML<T>(string path)
+        {
+            var xsSubmit = new XmlSerializer(typeof(T));
+            using (var reader = new FileStream(path, FileMode.Open))
+            {
+                return (T)xsSubmit.Deserialize(reader);
+            }
+        }
+    }
+}
