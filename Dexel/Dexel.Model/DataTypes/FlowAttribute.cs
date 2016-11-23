fe2e5eda69e82bf7495c54b74e21cde725761074
Dennis Müller <dennis.briefkasten@gmail.com>
2016-11-23 10:34:15 +0100

    commit

---
+++ Dexel/Dexel.Model/DataTypes/FlowAttribute.cs
@@ -0,0 +1,19 @@
+﻿
+using PropertyChanged;
+
+namespace Dexel.Model.DataTypes
+{
+
+    [ImplementPropertyChanged]
+    public class FlowAttribute
+    {
+        public string Name { get; set; }
+        public FlowAttributeType Type { get; set; }
+    }
+
+    public enum FlowAttributeType
+    {
+        State,
+        Provider
+    }
+}
