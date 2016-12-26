d266eaa683d0c802de5746023e02b14c0bba39ab
Dennis Müller <dennis.briefkasten@gmail.com>
2016-12-26 02:39:58 +0100

    commit

---
+++ Dexel/RoslynTests1/Generators/IntegrationGeneratorTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Roslyn;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roslyn.Tests
+{
+    [TestClass()]
+    public class IntegrationGeneratorTests
+    {
+        [TestMethod()]
+        public void TravelOutputsTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
